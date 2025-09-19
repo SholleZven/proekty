48fad67cdabbcc5f0ca48a7c733a1ac73cd86ec7
Роман Владиславович Серебряков <rvs@ge74.ru>
2025-09-19 14:55:22 +0500

    fix_index.blade.php

--- proekty/resources/views/index.blade.php
+++ proekty/resources/views/index.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="UTF-8">
-    <title>Проекты</title>
+    <title>Статус</title>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
