c60883bd1621e6a8549a2abd60947ce37a0a9858
Роман Владиславович Серебряков <rvs@ge74.ru>
2025-10-21 17:25:37 +0500

    add_yandex_metrika

--- proekty/resources/views/index.blade.php
+++ proekty/resources/views/index.blade.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0  shrink-to-fit=no">
     <link rel="shortcut icon" type="img/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <!-- Yandex.Metrika counter -->
+    <script
+        type="text/javascript">     (function (m, e, t, r, i, k, a) { m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) }; m[i].l = 1 * new Date(); for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } } k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a) })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=104764902', 'ym'); ym(104764902, 'init', { ssr: true, clickmap: true, accurateTrackBounce: true, trackLinks: true }); </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/104764902" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
     <title>Статус</title>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="app"></div>
 </body>
+
 </html>
